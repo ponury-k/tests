e90c4c03556320d730637bdd1901549112892b24
Michał Żaloudik <ponury.kostek@gmail.com>
2017-05-30 11:02:41 +0200

    test mongo version

--- test/test.js
+++ test/test.js
@@ -1,73 +1,19 @@
 "use strict";
-var assert = require("assert");
-var Utils = {
-	getType: function (val) {
-		return typeof val;
-	}
-};
-
-describe("getType", function () {
-	it("should be boolean", function () {
-		assert.equal(Utils.getType(true), "boolean");
+const assert = require("assert");
+const MongoClient = require('mongodb').MongoClient;
+const semver = require('semver');
+describe("mongodb", function () {
+	it("version", function (done) {
+		MongoClient.connect('mongodb://localhost:27017/test', function (err, db) {
+			assert.ok(err === null);
+			// Use the admin database for the operation
+			const adminDb = db.admin();
+			// Retrive the build information using the admin command
+			adminDb.command({buildInfo : 1}, function (err, info) {
+				assert.ok(err === null);
+				assert.ok(semver.satisfies(info.version, '>=3.4'));
+				done();
+			});
+		});
 	});
-	it("should be boolean", function () {
-		assert.equal(Utils.getType(false), "boolean");
-	});
-	it("should be integer", function () {
-		assert.equal(Utils.getType(0), "number");
-	});
-	it("should be integer", function () {
-		assert.equal(Utils.getType(1), "number");
-	});
-	it("should be float", function () {
-		assert.equal(Utils.getType(1.23), "number");
-	});
-	it("should be integer", function () {
-		assert.equal(Utils.getType(-1), "number");
-	});
-	it("should be float", function () {
-		assert.equal(Utils.getType(-1.23), "number");
-	});
-	it("should be string", function () {
-		assert.equal(Utils.getType('0'), "string");
-	});
-	it("should be string", function () {
-		assert.equal(Utils.getType('dumb'), "string");
-	});
-	/*it("should be array", function () {
-		assert.equal(Utils.getType([]), "array");
-	});
-	it("should be object", function () {
-		assert.equal(Utils.getType({}), "object");
-	});
-	it("should be Function", function () {
-		assert.equal(Utils.getType(function () { }), "function");
-	});
-	it("should be someFunction", function () {
-		assert.equal(Utils.getType(function someFunction() { }), "someFunction");
-	});
-	it("should be Date", function () {
-		assert.equal(Utils.getType(new Date()), "Date");
-	});
-	it("should be RegExp", function () {
-		assert.equal(Utils.getType(/regexp/), "RegExp");
-	});
-	it("should be Utils", function () {
-		assert.equal(Utils.getType(Utils), "Utils");
-	});
-	it("should be someFunction", function () {
-		function someFunction() {
-			return true;
-		}
-		assert.equal(Utils.getType(someFunction), "someFunction");
-	});
-	it("should be Null", function () {
-		assert.equal(Utils.getType(null), "null");
-	});
-	it("should be Undefined", function () {
-		assert.equal(Utils.getType(undefined), "undefined");
-	});
-	it("should be Float", function () {
-		assert.equal(Utils.getType(Infinity), "float");
-	});*/
 });
